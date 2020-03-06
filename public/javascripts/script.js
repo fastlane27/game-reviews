24c70769c6e96681ba759f9487573d8bf361b808
David Lane <fastlane27@gmail.com>
2020-03-05 19:57:15 -0600

    Partial styling

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,7 +2,7 @@ $('.edit-form').hide();
 $('body').click(toggleEdit);
 
 function toggleEdit(evt) {
-    if (evt.target.className !== 'edit-btn') return;
+    if (evt.target.id !== 'edit-btn') return;
     let btnId = evt.target.dataset.id;
     $(`.edit-form[data-id="${btnId}"]`).toggle();
 }
